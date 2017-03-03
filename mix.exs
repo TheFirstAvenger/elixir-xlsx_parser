4360528ab83f0746fdc038decc26003b9f52a053
Mike Binns <mike@mikebinns.net>
2017-03-03 11:00:33 -0500

    v0.0.10 added inline string parsing

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule XlsxParser.Mixfile do
 
   def project do
     [app: :xlsx_parser,
-     version: "0.0.9",
+     version: "0.0.10",
      elixir: "~> 1.0",
      description: description(),
      package: package(),
